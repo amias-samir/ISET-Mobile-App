c8a32ff7aa54b7de5f2cdfda06889116e08f8c66
Samir Dangal <smdangal201@gmail.com>
2018-12-04 16:30:21 +0545

    emergency contact activity created

---
+++ app/src/main/java/np/com/naxa/iset/emergencynumbers/EmergencyNumbersActivity.java
@@ -0,0 +1,15 @@
+package np.com.naxa.iset.emergencynumbers;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import np.com.naxa.iset.R;
+
+public class EmergencyNumbersActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_emergency_numbers);
+    }
+}
