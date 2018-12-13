f21eb01d32d888cb834cfdb4097a0ab2f7c5332f
Samir Dangal <smdangal201@gmail.com>
2018-12-13 14:24:24 +0545

    nav drawer added

--- app/src/main/java/np/com/naxa/iset/newhomepage/SectionGridHomeActivity.java
+++ app/src/main/java/np/com/naxa/iset/newhomepage/SectionGridHomeActivity.java
@@ -3,20 +3,33 @@ package np.com.naxa.iset.newhomepage;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
 
 import java.util.ArrayList;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import butterknife.Optional;
 import np.com.naxa.iset.R;
 import np.com.naxa.iset.profile.municipalityprofile.MunicipalityProfileActivity;
+import np.com.naxa.iset.utils.imageutils.CircleTransform;
 import np.com.naxa.iset.utils.recycleviewutils.LinearLayoutManagerWithSmoothScroller;
 import np.com.naxa.iset.utils.recycleviewutils.RecyclerViewType;
 
@@ -29,9 +42,27 @@ public class SectionGridHomeActivity extends AppCompatActivity {
     Button btnReactQuickly;
     @BindView(R.id.btn_info)
     Button btnDisasterNews;
+    @BindView(R.id.toolbar_general)
+    Toolbar toolbar;
+    @Nullable
+    @BindView(R.id.appbar_general)
+    AppBarLayout appbar;
+    @BindView(R.id.imageView7)
+    ImageView imageView;
+    @BindView(R.id.nav_view)
+    NavigationView navigationView;
+    @BindView(R.id.drawer_layout)
+    DrawerLayout drawer;
 
     private RecyclerViewType recyclerViewType;
     private RecyclerView recyclerView;
+
+    private View navHeader;
+    private ImageView imgNavHeaderBg, imgProfile;
+    private TextView txtName, txtWebsite;
+
+    // index to identify current nav menu item
+    public static int navItemIndex = 0;
 
     public static void start(Context context) {
         Intent intent = new Intent(context, SectionGridHomeActivity.class);
@@ -43,6 +74,22 @@ public class SectionGridHomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_section_grid_home);
         ButterKnife.bind(this);
+
+//        drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+//        navigationView = (NavigationView) findViewById(R.id.nav_view);
+
+        // Navigation view header
+        navHeader = navigationView.getHeaderView(0);
+        txtName = (TextView) navHeader.findViewById(R.id.name);
+        txtWebsite = (TextView) navHeader.findViewById(R.id.website);
+        imgNavHeaderBg = (ImageView) navHeader.findViewById(R.id.img_header_bg);
+        imgProfile = (ImageView) navHeader.findViewById(R.id.img_profile);
+
+        // load nav menu header data
+        loadNavHeader();
+
+        // initializing navigation menu
+        setUpNavigationView();
 
         recyclerViewType = RecyclerViewType.GRID;
 //        setUpToolbarTitle();
@@ -107,4 +154,117 @@ public class SectionGridHomeActivity extends AppCompatActivity {
                 break;
         }
     }
+
+    /***
+     * Load navigation menu header information
+     * like background image, profile image
+     * name, website, notifications action view (dot)
+     */
+    private void loadNavHeader() {
+        // name, website
+        txtName.setText("Samir Dangal");
+        txtWebsite.setText("samirdangal@gmail.com");
+
+        // loading header background image
+//        Glide.with(this).load(urlNavHeaderBg)
+//                .crossFade()
+//                .diskCacheStrategy(DiskCacheStrategy.ALL)
+//                .into(imgNavHeaderBg);
+
+        // Loading profile image
+        Glide.with(this).load("https://thumbs.dreamstime.com/b/profile-icon-male-avatar-portrait-casual-person-silhouette-face-flat-design-vector-46846328.jpg")
+                .crossFade()
+                .thumbnail(0.5f)
+                .bitmapTransform(new CircleTransform(this))
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                .into(imgProfile);
+
+        // showing dot next to notifications label
+//        navigationView.getMenu().getItem(3).setActionView(R.layout.menu_dot);
+    }
+
+    private void setUpNavigationView() {
+        //Setting Navigation View Item Selected Listener to handle the item click of the navigation menu
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+
+            // This method will trigger on item Click of navigation menu
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+
+                //Check to see which item was being clicked and perform appropriate action
+                switch (menuItem.getItemId()) {
+                    //Replacing the main content with ContentFragment Which is our Inbox View;
+                    case R.id.nav_home:
+                        navItemIndex = 0;
+                        break;
+                    case R.id.nav_profile:
+                        navItemIndex = 1;
+                        break;
+                    case R.id.nav_ask_for_help:
+                        navItemIndex = 2;
+                        break;
+                    case R.id.nav_report:
+                        navItemIndex = 3;
+                        break;
+                    case R.id.nav_notify_oithers:
+                        navItemIndex = 4;
+                        break;
+                    case R.id.nav_hazard_info:
+                        // launch new intent instead of loading fragment
+                        drawer.closeDrawers();
+                        return true;
+                    case R.id.nav_play_quiz:
+                        // launch new intent instead of loading fragment
+                        drawer.closeDrawers();
+                        return true;
+
+                    case R.id.nav_settings:
+                        // launch new intent instead of loading fragment
+                        drawer.closeDrawers();
+                        return true;
+
+                    case R.id.nav_map:
+                        // launch new intent instead of loading fragment
+                        drawer.closeDrawers();
+                        return true;
+                    default:
+                        navItemIndex = 0;
+                }
+
+                //Checking if the item is in checked state or not, if not make it in checked state
+                if (menuItem.isChecked()) {
+                    menuItem.setChecked(false);
+                } else {
+                    menuItem.setChecked(true);
+                }
+                menuItem.setChecked(true);
+
+                return true;
+            }
+        });
+
+
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.openDrawer, R.string.closeDrawer) {
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                // Code here will be triggered once the drawer closes as we dont want anything to happen so we leave this blank
+                super.onDrawerClosed(drawerView);
+            }
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                // Code here will be triggered once the drawer open as we dont want anything to happen so we leave this blank
+                super.onDrawerOpened(drawerView);
+            }
+        };
+
+        //Setting the actionbarToggle to drawer layout
+        drawer.setDrawerListener(actionBarDrawerToggle);
+
+        //calling sync state is necessary or else your hamburger icon wont show up
+        actionBarDrawerToggle.syncState();
+    }
+
+
 }
