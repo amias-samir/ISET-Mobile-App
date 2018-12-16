afda803586cdab01bbabed959eb902c6dc3523e8
Samir Dangal <smdangal201@gmail.com>
2018-12-16 15:58:55 +0545

    listner added to settings page

--- app/src/main/java/np/com/naxa/iset/settings/SettingsActivity.java
+++ app/src/main/java/np/com/naxa/iset/settings/SettingsActivity.java
@@ -3,6 +3,8 @@ package np.com.naxa.iset.settings;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.CompoundButton;
 import android.widget.Spinner;
@@ -12,6 +14,7 @@ import android.widget.Toast;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import np.com.naxa.iset.R;
+import np.com.naxa.iset.utils.SharedPreferenceUtils;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -22,6 +25,8 @@ public class SettingsActivity extends AppCompatActivity {
     @BindView(R.id.spinnerChooseLanguage)
     Spinner spinnerChooseLanguage;
 
+    SharedPreferenceUtils sharedPreferenceUtils;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,6 +34,8 @@ public class SettingsActivity extends AppCompatActivity {
         ButterKnife.bind(this);
 
         setupToolBar();
+
+        sharedPreferenceUtils = new SharedPreferenceUtils(SettingsActivity.this);
 
         setUpSpinner();
 
@@ -51,6 +58,20 @@ public class SettingsActivity extends AppCompatActivity {
         wardAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinnerChooseLanguage.setAdapter(wardAdapter);
 
+
+        spinnerChooseLanguage.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                sharedPreferenceUtils.setValue(SharedPreferenceUtils.APP_LANGUAGE, wardAdapter.getItem(position));
+                Toast.makeText(SettingsActivity.this, "Your default app language will be "+wardAdapter.getItem(position), Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
     }
 
     private void setSwitchButtonListner(){
@@ -58,8 +79,10 @@ public class SettingsActivity extends AppCompatActivity {
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 // do something, the isChecked will be
                 if(isChecked == true){
+                    sharedPreferenceUtils.setValue(SharedPreferenceUtils.IS_NOTIFY_ME, true);
                     Toast.makeText(SettingsActivity.this, "We will notify you soon.", Toast.LENGTH_SHORT).show();
                 }else {
+                    sharedPreferenceUtils.setValue(SharedPreferenceUtils.IS_NOTIFY_ME, false);
                     Toast.makeText(SettingsActivity.this, "We won't notify you.", Toast.LENGTH_SHORT).show();
 
                 }
