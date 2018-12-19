c9183fa55f6a8d11d4d552a9a4be7235cff6ebc6
Samir Dangal <smdangal201@gmail.com>
2018-12-19 16:04:05 +0545

    quiz category list item ui issue fixes

--- app/src/main/java/np/com/naxa/iset/quiz/ItemRecyclerViewQuizAdapter.java
+++ app/src/main/java/np/com/naxa/iset/quiz/ItemRecyclerViewQuizAdapter.java
@@ -6,6 +6,7 @@ import android.graphics.Color;
 import android.graphics.PorterDuff;
 import android.graphics.PorterDuffColorFilter;
 import android.graphics.drawable.Drawable;
+import android.graphics.drawable.GradientDrawable;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.graphics.drawable.DrawableCompat;
 import android.support.v7.widget.CardView;
@@ -25,6 +26,7 @@ import java.util.Random;
 
 import np.com.naxa.iset.R;
 import np.com.naxa.iset.event.EmergenctContactCallEvent;
+import np.com.naxa.iset.utils.colorutils.ColorList;
 
 /**
  * Created by samir on 01/12/18.
@@ -65,28 +67,17 @@ public class ItemRecyclerViewQuizAdapter extends RecyclerView.Adapter<ItemRecycl
     @Override
     public void onBindViewHolder(ItemViewHolder holder, int position) {
 
-        Random rnd = new Random();
-        int color = Color.argb(255, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256));
+//        Random rnd = new Random();
+//        int color = Color.argb(255, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256));
 
         holder.itemLabel.setText(arrayList.get(position));
-        holder.itemLabel.setTextColor(color);
+        holder.itemLabel.setTextColor(ColorList.COLORFUL_COLORS[position]);
 
-//        DrawableCompat.setTint(holder.item_bg.getDrawable(), color);
+        DrawableCompat.setTint(holder.item_bg.getBackground().mutate(), ColorList.COLORFUL_COLORS[position]);
 
-//        Drawable icon = ContextCompat.getDrawable(context, R.drawable.circular_non_solid_vector).mutate();
-//        TypedValue typedValue = new TypedValue();
-//       context.getTheme().resolveAttribute(color, typedValue, true);
-//        icon.setColorFilter(typedValue.data, PorterDuff.Mode.SRC_ATOP);
-//        holder.item_bg.setBackground(icon);
-
-//        Drawable mDrawable = context.getResources().getDrawable(R.drawable.circular_non_solid_vector).mutate();
-//        mDrawable.setColorFilter(new
-//                PorterDuffColorFilter(color,PorterDuff.Mode.MULTIPLY));
-//        holder.item_bg.setBackgroundDrawable(mDrawable);
-
-        Drawable mDrawable1 = context.getResources().getDrawable(R.drawable.button_rounded_purple).mutate();
+        Drawable mDrawable1 = context.getResources().getDrawable(R.drawable.button_rounded_purple).mutate().getCurrent();
         mDrawable1.setColorFilter(new
-                PorterDuffColorFilter(color,PorterDuff.Mode.MULTIPLY));
+                PorterDuffColorFilter(ColorList.COLORFUL_COLORS[position],PorterDuff.Mode.SRC));
        holder.quiz_percentage.setBackground(mDrawable1);
 
 
